afdaebacc9022e48a26831dc0492b9e880a6841e
Michael Santoro <michael.santoro@du.edu>
2023-02-09 19:54:15 -0700

    Modifying Runner

--- runner.py
+++ runner.py
@@ -23,7 +23,7 @@ from captum.attr import NoiseTunnel
 from captum.attr import visualization as viz
 
 from matplotlib.colors import LinearSegmentedColormap
-from hmdata import RawData,HMDataFactory
+from hmdata import RawData, HMFullAugLabelledData
 from img_transforms import StandardTransform, DataAugmentTransform
 from ic_template_models import initialize_model
 
